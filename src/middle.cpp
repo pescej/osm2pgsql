b3936d9cdfcf0f31e7e8126e4a7b40b766042e56
Jochen Topf <jochen@topf.org>
2021-05-05 11:03:06 +0200

    Add new experimental middle implementations: db
    This commits adds a new middle implementations 'db' which is similar to
    'pgsql'. All data is stored in the database. A new table structure with
    JSONB for tags and relation members is used.
    
    This is EXPERIMENTAL! Not well tested, not complete and performance
    might be horrible. It is intended to start a discussion about what we
    need and how to best implement it.
    
    To enable these, set the environment variable OSM2PGSQL_MIDDLE to either
    'db' or 'new'. If set to 'db' use of the new middle is forced,
    independent of the '--slim' setting! Or you can set it to 'new' in which
    case the 'db' middle will be used instead of the 'pgsql' middle if and
    when that would have been used.
    
    This way of setting the middle is temporary and gives us flexibility
    while experimenting, it will not be available in this form in the final
    implementation.
    
    The new middle has various options that can be set with the
    OSM2PGSQL_MIDDLE_OPTS environment variable. Use a comma separated list
    of options, a leading '-' disables the option.
    
    Options:
    * drop_tables
    * forward_dependencies
    * has_bucket_index
    * tags
    * attributes
    * untagged_nodes
    * locations
    * way_nodes
    * relation_members

--- src/middle.cpp
+++ src/middle.cpp
@@ -7,6 +7,11 @@
  * For a full list of authors see the git log.
  */
 
+#include <cstdlib>
+#include <cstring>
+
+#include "logging.hpp"
+#include "middle-db.hpp"
 #include "middle-pgsql.hpp"
 #include "middle-ram.hpp"
 #include "middle.hpp"
@@ -15,6 +20,29 @@
 std::shared_ptr<middle_t>
 create_middle(options_t const &options, thread_pool_t *thread_pool)
 {
+    // hack to enable specific middle for testing
+    char const *const middle_name = std::getenv("OSM2PGSQL_MIDDLE");
+    if (middle_name) {
+        log_debug("Middle set to '{}' from OSM2PGSQL_MIDDLE env var",
+                  middle_name);
+        if (std::strcmp(middle_name, "db") == 0) {
+            return std::make_shared<middle_db_t>(&options, thread_pool);
+        }
+        if (std::strcmp(middle_name, "pgsql") == 0) {
+            return std::make_shared<middle_pgsql_t>(&options, thread_pool);
+        }
+        if (std::strcmp(middle_name, "ram") == 0) {
+            return std::make_shared<middle_ram_t>(&options, thread_pool);
+        }
+        if (std::strcmp(middle_name, "new") == 0) {
+            if (options.slim) {
+                return std::make_shared<middle_db_t>(&options, thread_pool);
+            }
+            return std::make_shared<middle_ram_t>(&options, thread_pool);
+        }
+        throw std::runtime_error{"Unknown middle '{}'"_format(middle_name)};
+    }
+
     if (options.slim) {
         return std::make_shared<middle_pgsql_t>(&options, thread_pool);
     }
